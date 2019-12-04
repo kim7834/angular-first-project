32ad1e713834cf311d1eb7d6fdf945eda3eb65c9
kim7834 <56540017+kim7834@users.noreply.github.com>
2019-12-04 10:19:34 +0100

    Add files via upload
    src/app

---
+++ app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationModule } from './navigation/navigation.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NavigationModule, HttpClientModule, FormsModule ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
